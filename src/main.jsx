0711e0fb028a5e1a0af92a19f67dc8befae6097f
Jin19121 <jinkim19121@gmail.com>
2024-11-06 11:40:46 +0900

    list map 활용

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,9 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import MeApp9 from "./practice/MeApp9.jsx";
+import MyApp10 from "./apps/MyApp10.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <MeApp9 />
+    <MyApp10 />
   </StrictMode>,
 );
