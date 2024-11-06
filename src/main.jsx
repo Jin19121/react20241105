08c8eb400b5756a57750e9267d1ba929c655c965
Jin19121 <jinkim19121@gmail.com>
2024-11-06 15:26:51 +0900

    spread

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,9 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import MeApp19 from "./practice/MeApp19.jsx";
+import MyApp20 from "./apps/MyApp20.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <MeApp19 />
+    <MyApp20 />
   </StrictMode>,
 );
