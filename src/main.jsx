5ec9c25138135c3344d75eb1391b9b2c89b09339
Jin19121 <jinkim19121@gmail.com>
2024-11-06 16:42:17 +0900

    chakra ui 맛보기 (버튼)

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,12 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import MeApp24 from "./practice/MeApp24.jsx";
 import { Provider } from "./components/ui/provider.jsx";
+import MyApp25 from "./apps/MyApp25.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <Provider>
-      <MeApp24 />
+      <MyApp25 />
     </Provider>
   </StrictMode>,
 );
