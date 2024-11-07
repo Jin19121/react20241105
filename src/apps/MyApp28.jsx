10d6e4a79278ab8f1fc7527d954871ab6ca9fe92
Jin19121 <jinkim19121@gmail.com>
2024-11-07 10:15:46 +0900

    chakra textarea

--- src/apps/MyApp28.jsx
+++ src/apps/MyApp28.jsx
@@ -1,17 +1,18 @@
 import React from "react";
-import { Input } from "@chakra-ui/react";
+import { Input, Textarea } from "@chakra-ui/react";
 import { Field } from "../components/ui/field.jsx";
 
 function MyApp28(props) {
   return (
-    <div>
+    <div style={{ margin: "10px" }}>
+      <Textarea placeholder={"자기소개 1000자 제한"}></Textarea>
+      <hr />
       <Field label={"Email"} required helperText={"유요한 이메일 입력"}>
         <Input />
       </Field>
       <Input variant={"subtle"} placeholder={"email"} />
       <Input variant={"outline"} placeholder={"address"} />
       <Input variant={"flushed"} placeholder={"name"} />
-      <br />
       <hr />
       <input type="text" placeholder={"****@email.com"} />
     </div>
