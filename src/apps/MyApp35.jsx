af2fe82a573907b5d6945273894c930117ed17fe
Jin19121 <jinkim19121@gmail.com>
2024-11-07 14:38:41 +0900

    event keyup, click

---
+++ src/apps/MyApp35.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+import { Button } from "../components/ui/button.jsx";
+import { Input } from "@chakra-ui/react";
+
+function MyApp35(props) {
+  function fun1(e) {
+    console.log("clicked");
+    console.log(e.target);
+  }
+
+  function fun2(e) {
+    console.log(e.target.value);
+  }
+
+  return (
+    <div>
+      <Input onKeyUp={(e) => console.log(e.target.value)} />
+      <hr />
+      <Input onKeyUp={fun2} />
+      <Input onKeyUp={fun2} />
+      <Input onKeyUp={fun2} />
+      <hr />
+      <Button onClick={fun1}>버튼1</Button>
+      <Button onClick={fun1}>버튼2</Button>
+      <Button onClick={fun1}>버튼3</Button>
+    </div>
+  );
+}
+
+export default MyApp35;
