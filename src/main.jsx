eab83490dc5569ffe0a4846f5fce70b545cf6504
Jin19121 <jinkim19121@gmail.com>
2024-11-08 16:39:44 +0900

    context로 prop drilling 없이 두 자식의 데이터 전달

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,11 @@
 import { createRoot } from "react-dom/client";
 import { Provider } from "./components/ui/provider.jsx";
-import App50 from "./practice/App50.jsx";
+import MyApp51 from "./apps/MyApp51.jsx";
 
 createRoot(document.getElementById("root")).render(
   // <StrictMode>
   <Provider>
-    <App50 />
+    <MyApp51 />
   </Provider>,
   // </StrictMode>,
 );
