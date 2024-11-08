eefa673fc41342950d9252f76e202b1bf010c701
Jin19121 <jinkim19121@gmail.com>
2024-11-08 12:32:48 +0900

    re-render의 순간

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,11 @@
-import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { Provider } from "./components/ui/provider.jsx";
-import App44 from "./practice/App44.jsx";
+import MyApp45 from "./apps/MyApp45.jsx";
 
 createRoot(document.getElementById("root")).render(
-  <StrictMode>
-    <Provider>
-      <App44 />
-    </Provider>
-  </StrictMode>,
+  // <StrictMode>
+  <Provider>
+    <MyApp45 />
+  </Provider>,
+  // </StrictMode>,
 );
