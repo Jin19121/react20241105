31c6fe3a820813c0c6f4c2ab3dbce6c7d44b27fa
Jin19121 <jinkim19121@gmail.com>
2024-11-08 11:29:38 +0900

    중첩 객체 활용(미완성)

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,12 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { Provider } from "./components/ui/provider.jsx";
-import MyApp42 from "./apps/MyApp42.jsx";
+import App43 from "./practice/App43.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <Provider>
-      <MyApp42 />
+      <App43 />
     </Provider>
   </StrictMode>,
 );
---
+++ src/practice/App43.jsx
@@ -0,0 +1,76 @@
+import React, { useState } from "react";
+import { Input } from "@chakra-ui/react";
+import { Field } from "../components/ui/field.jsx";
+import { Button } from "../components/ui/button.jsx";
+
+const originPerson = {
+  name: "홍길동",
+  info: {
+    address: "한양",
+    email: "naver",
+    description: "의적",
+  },
+};
+function App43(props) {
+  const [person, setPerson] = useState(originPerson);
+  return (
+    <div>
+      <Field label={"이름"}>
+        <Input
+          value={person.name}
+          onChange={(e) => {
+            const newPerson = {
+              ...person,
+              name: e.target.value,
+              info: { ...person.info },
+            };
+            setPerson(newPerson);
+          }}
+        />
+      </Field>
+      <Field label={"주소"}>
+        <Input
+          value={person.info.address}
+          onClick={(e) => {
+            setPerson({
+              ...person,
+              info: { ...person.info, address: e.target.value },
+            });
+          }}
+        />
+      </Field>
+      <Field label={"이메일"}>
+        <Input
+          value={person.info.email}
+          onClick={(e) => {
+            setPerson({
+              ...person,
+              info: { ...person.info, email: e.target.value },
+            });
+          }}
+        />
+      </Field>
+      <Field label={"자기 소개"}>
+        <Input
+          value={person.info.description}
+          onClick={(e) => {
+            setPerson({
+              ...person,
+              info: { ...person.info, description: e.target.value },
+            });
+          }}
+        />
+      </Field>
+      <Button
+        onClick={() => setPerson(originPerson)}
+        colorPalette={"red"}
+        variant={"surface"}
+      >
+        {" "}
+        리셋
+      </Button>
+    </div>
+  );
+}
+
+export default App43;
