3fccb7f715c8519fa9af5f4ec7b4d727f73c03cc
Jin19121 <jinkim19121@gmail.com>
2024-11-11 15:20:01 +0900

    JSON사용할거임

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,12 @@
 import { createRoot } from "react-dom/client";
 import { Provider } from "./components/ui/provider.jsx";
 import { StrictMode } from "react";
-import MyApp59 from "./apps/MyApp59.jsx";
+import MyApp60 from "./apps/MyApp60.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <Provider>
-      <MyApp59 />
+      <MyApp60 />
     </Provider>
   </StrictMode>,
 );
