e02efb0150e55e557e6a2f288a063fb02f752585
Jin19121 <jinkim19121@gmail.com>
2024-11-11 14:01:38 +0900

    promise

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,12 @@
 import { createRoot } from "react-dom/client";
 import { Provider } from "./components/ui/provider.jsx";
-import MyApp55 from "./apps/MyApp55.jsx";
 import { StrictMode } from "react";
+import MyApp56 from "./apps/MyApp56.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <Provider>
-      <MyApp55 />
+      <MyApp56 />
     </Provider>
   </StrictMode>,
 );
