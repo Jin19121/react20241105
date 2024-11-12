a9ce47395102824f5050beccc582b06ec2280d81
Jin19121 <jinkim19121@gmail.com>
2024-11-12 11:13:11 +0900

    put, delete 요청

--- src/apps/MyApp63.jsx
+++ src/apps/MyApp63.jsx
@@ -1,7 +1,81 @@
 import React from "react";
+import { Button } from "../components/ui/button.jsx";
+import axios from "axios";
 
 function MyApp63(props) {
-  return <div></div>;
+  return (
+    <div>
+      <Button
+        onClick={() => {
+          axios.delete("/api/main3/sub8", {
+            data: {
+              id: 5,
+              name: "moly",
+            },
+          });
+        }}
+      >
+        Click Delete
+      </Button>
+      <Button
+        onClick={() => {
+          axios.put("/api/main3/sub7", { title: "도겸 시점", author: "DK" });
+        }}
+      >
+        Click Put
+      </Button>
+      <hr />
+      <Button
+        onClick={() => {
+          axios.delete("/api/main3/sub6", {
+            data: {
+              id: 5,
+              name: "kim",
+            },
+          });
+        }}
+      >
+        delete (w/ json)
+      </Button>
+      <Button
+        onClick={() => {
+          axios.delete("/api/main3/sub5");
+        }}
+      >
+        delete1
+      </Button>
+      <hr />
+      <Button
+        onClick={() => {
+          axios.put("/api/main3/sub4", { title: "제목", author: "한강" });
+        }}
+      >
+        put (w/ json)
+      </Button>
+      <Button
+        onClick={() => {
+          axios.put("/api/main3/sub3");
+        }}
+      >
+        put1
+      </Button>
+      <hr />
+      <Button
+        onClick={() => {
+          axios.post("/api/main3/sub2", { name: "dk", age: 28 });
+        }}
+      >
+        post1
+      </Button>
+      <Button
+        onClick={() => {
+          axios.get("/api/main3/sub1");
+        }}
+      >
+        get1
+      </Button>
+    </div>
+  );
 }
 
 export default MyApp63;
--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,12 @@
 import { createRoot } from "react-dom/client";
 import { Provider } from "./components/ui/provider.jsx";
 import { StrictMode } from "react";
-import MyApp62 from "./apps/MyApp62.jsx";
+import MyApp63 from "./apps/MyApp63.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <Provider>
-      <MyApp62 />
+      <MyApp63 />
     </Provider>
   </StrictMode>,
 );
