e5f1b56feb26a697d2fa8eafb23bc1d53bb6c9cc
Jin19121 <jinkim19121@gmail.com>
2024-11-12 09:26:24 +0900

    post 요청 : javaBean으로 받음

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,12 @@
 import { createRoot } from "react-dom/client";
 import { Provider } from "./components/ui/provider.jsx";
 import { StrictMode } from "react";
-import MyApp54 from "./apps/MyApp54.jsx";
+import MyApp62 from "./apps/MyApp62.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <Provider>
-      <MyApp54 />
+      <MyApp62 />
     </Provider>
   </StrictMode>,
 );
