8f96303a2526a5c05ebc3500de33ee4da463de8f
Jin19121 <jinkim19121@gmail.com>
2024-11-12 15:40:16 +0900

    response data: database포함

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,12 @@
 import { createRoot } from "react-dom/client";
 import { Provider } from "./components/ui/provider.jsx";
 import { StrictMode } from "react";
-import MyApp65 from "./apps/MyApp65.jsx";
+import MyApp66 from "./apps/MyApp66.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <Provider>
-      <MyApp65 />
+      <MyApp66 />
     </Provider>
   </StrictMode>,
 );
