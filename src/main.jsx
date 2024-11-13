9ce97834ac419414c1640381bd0bdf4ee960317d
Jin19121 <jinkim19121@gmail.com>
2024-11-13 10:57:07 +0900

    파일 전송

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,12 @@
 import { createRoot } from "react-dom/client";
 import { Provider } from "./components/ui/provider.jsx";
 import { StrictMode } from "react";
-import MyApp68 from "./apps/MyApp68.jsx";
+import MyApp69 from "./apps/MyApp69.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <Provider>
-      <MyApp68 />
+      <MyApp69 />
     </Provider>
   </StrictMode>,
 );
