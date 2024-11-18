18967df9bf2ffa1e23bb2b8d7b200bbb453d2b78
Jin19121 <jinkim19121@gmail.com>
2024-11-18 17:35:20 +0900

    token 반환

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,12 @@
 import { createRoot } from "react-dom/client";
 import { Provider } from "./components/ui/provider.jsx";
 import { StrictMode } from "react";
-import MyApp75 from "./apps/MyApp75.jsx";
+import MyApp80 from "./apps/MyApp80.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <Provider>
-      <MyApp75 />
+      <MyApp80 />
     </Provider>
   </StrictMode>,
 );
